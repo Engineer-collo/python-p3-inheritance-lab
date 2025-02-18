83e107b913a809b16ab1256c12ee50017b83ddc8
Engineer-Collo <collinslikhomba@gmail.com>
2025-02-18 21:05:26 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize with an empty knowledge list
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
